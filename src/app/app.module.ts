010db8f5de0ab0602f7906d5056dce2ee3495173
Jacob Ajao <jacob.ajao@fcmb.com>
2021-02-17 21:41:31 +0100

    Initial Acknowledgement page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ToastrModule } from "ngx-toastr";
 
@@ -12,12 +12,20 @@ import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-
+import { MatFormFieldModule  } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatNativeDateModule } from "@angular/material/core";
+import { DispatchcardComponent } from "./pages/dispatchcard/dispatchcard.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLayoutComponent,
+    AdminLayoutComponent
+    
   ],
   imports: [
     BrowserAnimationsModule,
@@ -25,11 +33,30 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
       useHash: true
     }),
     SidebarModule,
-    NavbarModule,
+    NavbarModule,    
+    MatFormFieldModule,
     ToastrModule.forRoot(),
     FooterModule,
-    FixedPluginModule
+    FixedPluginModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    //HttpClientModule,
+    //DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    //MatSelectModule,
+    //MatButtonModule,
+    //MatInputModule
+
   ],
+  exports: [    
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule  
+  ],  
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -18,13 +18,25 @@ import { AcknowledgeComponent }    from '../../pages/Acknowledge/Acknowledge.com
 import { ActivateComponent }    from '../../pages/Activate/Activate.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(AdminLayoutRoutes),
     FormsModule,
-    NgbModule
+    NgbModule,
+    MatFormFieldModule,    
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatTableModule 
   ],
   declarations: [
     DashboardComponent,
@@ -38,7 +50,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     DispatchcardComponent,
     AcknowledgeComponent,
     ActivateComponent
-  ]
+  ],
+  exports: [    
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule  
+  ], 
 })
 
 export class AdminLayoutModule {}
